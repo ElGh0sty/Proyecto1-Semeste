19281232f28269ae66452f24845408f52ae7124d
Ghost <165936076+ElGh0sty@users.noreply.github.com>
2025-08-29 22:03:49 -0500

    Add files via upload

---
+++ Proyecto1ºSemestre/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Proyecto1ºSemestre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextBox textBox1;
+            TextBox textBox2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            hopePictureBox1 = new ReaLTaiizor.Controls.HopePictureBox();
+            bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+            bigLabel2 = new ReaLTaiizor.Controls.BigLabel();
+            pictureBox1 = new PictureBox();
+            button2 = new ReaLTaiizor.Controls.Button();
+            bigLabel3 = new ReaLTaiizor.Controls.BigLabel();
+            bigLabel4 = new ReaLTaiizor.Controls.BigLabel();
+            button1 = new ReaLTaiizor.Controls.Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)hopePictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(428, 223);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Usuario";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 9;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(428, 282);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Contraseña";
+            textBox2.Size = new Size(145, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // hopePictureBox1
+            // 
+            hopePictureBox1.BackColor = Color.FromArgb(64, 64, 64);
+            hopePictureBox1.Location = new Point(296, 12);
+            hopePictureBox1.Name = "hopePictureBox1";
+            hopePictureBox1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            hopePictureBox1.Size = new Size(408, 529);
+            hopePictureBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            hopePictureBox1.TabIndex = 5;
+            hopePictureBox1.TabStop = false;
+            hopePictureBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            // 
+            // bigLabel1
+            // 
+            bigLabel1.AutoSize = true;
+            bigLabel1.BackColor = Color.FromArgb(64, 64, 64);
+            bigLabel1.Font = new Font("Yu Gothic UI", 24.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bigLabel1.ForeColor = Color.LavenderBlush;
+            bigLabel1.Location = new Point(331, 62);
+            bigLabel1.Name = "bigLabel1";
+            bigLabel1.Size = new Size(343, 45);
+            bigLabel1.TabIndex = 6;
+            bigLabel1.Text = "¡Bienvenido a ArtLab!";
+            bigLabel1.Click += bigLabel1_Click;
+            // 
+            // bigLabel2
+            // 
+            bigLabel2.AutoSize = true;
+            bigLabel2.BackColor = Color.FromArgb(64, 64, 64);
+            bigLabel2.Font = new Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bigLabel2.ForeColor = Color.LavenderBlush;
+            bigLabel2.Location = new Point(428, 168);
+            bigLabel2.Name = "bigLabel2";
+            bigLabel2.Size = new Size(145, 32);
+            bigLabel2.TabIndex = 7;
+            bigLabel2.Text = "Inicia sesion";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.LightPink;
+            pictureBox1.Location = new Point(404, 262);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(194, 4);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightPink;
+            button2.BorderColor = Color.FromArgb(255, 192, 255);
+            button2.EnteredBorderColor = Color.FromArgb(255, 192, 255);
+            button2.EnteredColor = Color.FromArgb(255, 192, 255);
+            button2.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Image = null;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.InactiveColor = Color.LightPink;
+            button2.Location = new Point(441, 332);
+            button2.Name = "button2";
+            button2.PressedBorderColor = Color.Pink;
+            button2.PressedColor = Color.LightPink;
+            button2.Size = new Size(120, 40);
+            button2.TabIndex = 15;
+            button2.Text = "Inicia Sesion";
+            button2.TextAlignment = StringAlignment.Center;
+            button2.Click += button2_Click;
+            // 
+            // bigLabel3
+            // 
+            bigLabel3.AutoSize = true;
+            bigLabel3.BackColor = Color.FromArgb(64, 64, 64);
+            bigLabel3.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bigLabel3.ForeColor = Color.LavenderBlush;
+            bigLabel3.Location = new Point(428, 383);
+            bigLabel3.Name = "bigLabel3";
+            bigLabel3.Size = new Size(147, 21);
+            bigLabel3.TabIndex = 16;
+            bigLabel3.Text = "¿No tienes cuenta?";
+            // 
+            // bigLabel4
+            // 
+            bigLabel4.AutoSize = true;
+            bigLabel4.BackColor = Color.Transparent;
+            bigLabel4.Font = new Font("Segoe UI", 25F);
+            bigLabel4.ForeColor = Color.FromArgb(80, 80, 80);
+            bigLabel4.Location = new Point(775, 262);
+            bigLabel4.Name = "bigLabel4";
+            bigLabel4.Size = new Size(0, 46);
+            bigLabel4.TabIndex = 17;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightPink;
+            button1.BorderColor = Color.FromArgb(255, 192, 255);
+            button1.EnteredBorderColor = Color.FromArgb(255, 192, 255);
+            button1.EnteredColor = Color.FromArgb(255, 192, 255);
+            button1.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Image = null;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.InactiveColor = Color.LightPink;
+            button1.Location = new Point(441, 417);
+            button1.Name = "button1";
+            button1.PressedBorderColor = Color.Pink;
+            button1.PressedColor = Color.LightPink;
+            button1.Size = new Size(120, 40);
+            button1.TabIndex = 18;
+            button1.Text = "Registrate";
+            button1.TextAlignment = StringAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(64, 64, 64);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(298, 431);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(107, 108);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 19;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(64, 64, 64);
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(567, 396);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(135, 143);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 20;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(64, 64, 64);
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(468, 103);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(65, 62);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 21;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(710, -2);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(285, 317);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 22;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.Transparent;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(-13, 240);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(305, 327);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 23;
+            pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Pink;
+            ClientSize = new Size(976, 560);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(bigLabel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(bigLabel4);
+            Controls.Add(bigLabel3);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(bigLabel2);
+            Controls.Add(hopePictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)hopePictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ReaLTaiizor.Controls.HopePictureBox hopePictureBox1;
+        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+        private ReaLTaiizor.Controls.BigLabel bigLabel2;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.Button button2;
+        private ReaLTaiizor.Controls.BigLabel bigLabel3;
+        private ReaLTaiizor.Controls.BigLabel bigLabel4;
+        private ReaLTaiizor.Controls.Button button1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+    }
+}
